290dd3f6afaf23a059cb862b6d40d2790926fa83
sdasaa <a.sudhakardas@gmail.com>
2025-06-14 12:51:07 +0530

    Test runner jenkinsfile-3

--- runner.sh
+++ runner.sh
@@ -31,7 +31,7 @@ do
 			echo " The HUB is NOT ready even after 30 seconds, Aborting !! "
 			exit 1
 		fi
-	sleep 1
+	sleep 10
 done
 
 ######################## 3.Invokes test run using java cmd  ########################
